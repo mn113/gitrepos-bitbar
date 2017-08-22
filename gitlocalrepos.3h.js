b08c37eb4a4c2aaf02608c9c0a3ac4fa698e2f77
Martin Nicholson <recyclebing+github@gmail.com>
2017-08-22 17:41:19 +0100

    Extracting repo data, still not async

--- gitlocalrepos.3h.js
+++ gitlocalrepos.3h.js
@@ -4,15 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Git = require('nodegit');
 
-const basedir = '~/Dropbox/Python';
+// User config:
+const basedir = '~/Dropbox/Python';		// modify as desired
 const GET_REPOS_CMD = "find " + basedir + " -name .git";
+const excludes = ["^\.", "Library\/"];	// TODO
 
-var excludes = ["^\.", "Library\/"];
+var allRepoPaths = [];
+var allRepos = [];
 
 // Run a shell command to find all the .git directories:
-allRepoPaths = [];
 const exec = require('child_process').exec;
-const child = exec(GET_REPOS_CMD, (error, stdout, stderr) => {
+exec(GET_REPOS_CMD, (error, stdout, stderr) => {
     if (error) {
         console.error('stderr', stderr);
         throw error;
@@ -22,21 +24,54 @@ const child = exec(GET_REPOS_CMD, (error, stdout, stderr) => {
 
 	console.log("Local git repos:");
 	allRepoPaths.forEach(analyseRepo);
-
 });
 
+// Using nodegit, open up each repo and save its details to an object:
 function analyseRepo(repo_path) {
+	var repo = {
+		fullpath: repo_path
+	};
+
 	Git.Repository.open(repo_path)
 	.then(function(repository) {
+		// Extract repo info:
+		repo.remotes = [];
+		repo.dir = '';
+		repo.branch = '';
+		repo.totalCommits = 0;
+		// Move on to last commit:
 		return repository.getHeadCommit();	// Promise
 	})
 	.then(function(commit) {
-		console.log(repo_path);
-		console.log("Last commit:", commit.date(), commit.message(), commit.author().name());
-		return commit.sha();
+		// Extract last commit info:
+		repo.lastCommitDate = commit.date();
+		repo.lastCommitMessage = commit.message();
+		repo.lastCommitAuthor = commit.author().name();
+		console.log(repo);
+		allRepos.push(repo);
 	});
 }
 
+// Most recent ones first:
+function sortRecent(repos) {
+	return repos.sort(function(a,b) {
+		return b.lastCommitTime - a.lastCommitTime;
+	});
+}
+
+// Output:
+function output() {
+	sortRecent(allRepos).forEach(function(repo) {
+		// Format display for menubar:
+		console.log(repo.dir);
+		console.log("Last commit:", repo.lastCommitDate, "| size=12 color=#999");
+		console.log("--"+repo.lastCommitMessage);
+		console.log("--"+repo.branch);
+		console.log("--"+repo.author);
+		console.log("--"+repo.totalCommits, "total commits");
+		console.log("--Open in iTerm");
+	});
+}
 
 // find all local git repos (under a certain folder) - OK
 // exclude unwanted
